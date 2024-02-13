5f4c90d6f7e1a4b365e0ca0e351e86c58fc64f92
KELLOU Lylia Ines <108697021+ineskel@users.noreply.github.com>
2024-02-13 09:37:21 +0100

    Add files via upload

---
+++ myApp.js
@@ -0,0 +1,43 @@
+let express = require('express');
+let app = express();
+console.log("Hello World");
+
+
+app.get('/', function(req, res) {
+    res.send('Response String');
+  });  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
